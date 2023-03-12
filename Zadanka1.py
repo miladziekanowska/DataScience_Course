2fa9bc8a6a365f7a241ab6fdf9c3a166bba7ece6
Miłosława Dziekanowska <miloslawadziekanowska@gmail.com>
2023-03-12 12:00:19 +0100

    Zadanka1.py added

---
+++ Zadanka1.py
@@ -0,0 +1,23 @@
+# 1. Utwórz zmienne: x i y oraz przypisz do nich dowolne liczby.
+#    Następnie wyświetl wszystkie operacje arytmetczne z wykorzystaniem zmiennych
+
+# 2. Utwórz zmienną tekstową (dowolny) tekst, a następnie dopisz do niego (do zmiennej)
+#    Tekst: "a kot ma pierdolca"
+
+#1
+
+x = 4
+y = 9.94
+
+print("Dodawanie: ", x + y, "\n",
+      "Odejmowanie: ", x - y, "\n",
+      "Mnożenie: ", x * y, "\n",
+      "Dzielenie: ", x / y, "\n",
+      "Potęgowanie: ", x ** y, "\n",
+      "Dzielenie bez reszty: ", x // y, "\n",
+      "Reszta z dzielenia: ", x % y)
+
+#2
+
+what = "Ala ma kota, "
+print(what, "kot ma pierdolca")
