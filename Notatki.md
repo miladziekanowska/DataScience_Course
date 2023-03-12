a522573ebc3899019433cba9b6f63a8e17f54139
Miłosława Dziekanowska <miloslawadziekanowska@gmail.com>
2023-03-12 13:34:25 +0100

    Dodano nowe zadania do Zadanka1.py, dodano pliki Notatki.md oraz Podstawy.py

---
+++ Notatki.md
@@ -0,0 +1,4 @@
+Python w Data Science 12.03.2023
+1. Podłączanie repozytorium do PyCharm (lub do innych, wypróbuj na Atomie) i updatowanie poprzez konsolę
+2. Typy danych (4 podstawowe) i dane input
+3. Konwersja danych (z jednego na drugi typ)
--- Zadanka1.py
+++ Zadanka1.py
@@ -7,7 +7,7 @@
 #1
 
 x = 4
-y = 9.94
+y = 5
 
 print("Dodawanie: ", x + y, "\n",
       "Odejmowanie: ", x - y, "\n",
@@ -20,4 +20,29 @@ print("Dodawanie: ", x + y, "\n",
 #2
 
 what = "Ala ma kota, "
-print(what, "kot ma pierdolca")
+what += "kot ma pierdolca"
+print(what)
+
+
+# 3. Napisać program, który przyjmie od użytkownika dwie wartości zmiennoprzecinkowe (float)
+#     a nastęnie wyświetli ich: sumę, różnicę oraz iloczyn
+
+# 4. Napisać program, który przyjmie od użytkownika tekst jako "nazwę produktu" oraz
+#     jego ilośc a następnie wyświetli komunikat w formacie:
+#     produkt - ilość
+
+#3
+
+value1 = input("Wprowadź pierwszą liczbę: ")
+value2 = input("Wprowadź drugą liczbę: ")
+value1 = float(value1)
+value2 = float(value2)
+print("Suma: ", value1 + value2,
+      "\nRóżnica: ", value1 - value2,
+      "\nIloczyn: ", value1 * value2)
+
+#4
+
+product = input("Wprowadź nazwę produktu: ")
+quantity = input("Wprowadź ilość: ")
+print(product, " - ", quantity)
