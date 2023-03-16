fb5b3b02f52797167122cbcb85834b6b733e55cd
Miłosława Dziekanowska <miloslawadziekanowska@gmail.com>
2023-03-16 20:22:11 +0100

    Dodano trzy zadania i ich rozwiazania do Zadanka4.py

--- Zadanka4.py
+++ Zadanka4.py
@@ -16,3 +16,34 @@ for l in range(len(slowo)):
         litery[slowo[l]] = litery[slowo[l]] + 1
 
 print(slowo, litery)
+
+
+# Napisz program, gdzie użytkownik podaje liczbę n (int)
+# Następnie program wyświetla wszystkie liczby parzyste od 0 do n (włącznie)
+
+liczba = int(input("Wprowadź swoją liczbę: "))
+n = 0
+while n <= liczba:
+    print(n)
+    n += 2
+
+# Wykorzystując pętle while, program wyświetli wszystkie pierwiastki
+#     liczb od 10 do 2 (włącznie) (n ** 0.5)
+
+a = 10
+while a in range(10, 1, -1):
+    print (a ** 0.5)
+    a -= 1
+
+# Napisz program, który sumuje wszystkie liczby całkowite z danego przedziału
+# Początek i koniec podaje użytkownik
+# Np. start = 10, end = 12, wynik - 33
+
+start_value = int(input("Start value: "))
+end_value = int(input("End value: "))
+start_sum = 0
+
+for b in range(start_value, end_value +1):
+     start_sum += b
+
+print(start_sum)
