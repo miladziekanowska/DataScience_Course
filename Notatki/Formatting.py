709b7a65f9ce9566d2d445f3820130d36ee6c113
Miłosława Dziekanowska <miloslawadziekanowska@gmail.com>
2023-04-01 23:33:29 +0200

    Some changes

--- Notatki/Formatting.py
+++
@@ -1,8 +0,0 @@
-#fprint z zaokrągleniem
-
-value = 9.3566225
-c = 300
-print(f"Wartość: {value:6.2f}") #6 oznacza tutaj ile znaków (spacji, liczb i kropek) zajmie nasza zmienna
-print(f"Wartość: {value:.2f}")
-print(f"Wartość {c:4d}")
-
